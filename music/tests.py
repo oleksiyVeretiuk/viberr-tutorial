ff62461c964aac22b242f8eb74a9a0e7670edea2
arden199866 <oleksiy.veretiuk@gmail.com>
2019-03-10 11:16:17 +0200

    Init commit

---
+++ music/tests.py
@@ -0,0 +1,14 @@
+# from django.test import TestCase
+#
+#
+# class AuthTestCase(TestCase):
+#
+#     def test_registration(self):
+#         pass
+#
+#     def test_login(self):
+#         pass
+#
+#
+# class MusicTestCase(TestCase):
+#     pass
